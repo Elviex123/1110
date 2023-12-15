ca6311887d5be622ac3fb9df9204b5ea750a54fa
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2023-12-15 09:52:53 +0800

    2

--- 1215/2.py
+++ 1215/2.py
@@ -1,3 +1,5 @@
 class Student:
-    pass
-S1=Student()
+    def __init__(self,id,name,major):
+        S1 = Student('stust','A','資工')
+        def save_to_database():
+            S1=save_to_database()
