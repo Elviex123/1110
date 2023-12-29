07981b18c0bbf9cdae00da04af995c6b7baa500e
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2023-12-29 10:16:00 +0800

    1

--- 1222/4.py
+++ 1222/4.py
@@ -9,23 +9,23 @@ class Course:
         self.course_time = course_time
 
     def print_course_info(self):
-        print(f"課程代碼: {self.course_id}")
-        print(f"課程名稱: {self.course_name}")
-        print(f"學分數: {self.course_hours}")
-        print(f"必選修: {self.course_elective}")
-        print(f"任課老師: {self.course_teacher}")
-        print(f"上課時間: {self.course_time}")
+            print(f"課程代碼: {self.course_id}")
+            print(f"課程名稱: {self.course_name}")
+            print(f"學分數: {self.course_hours}")
+            print(f"必選修: {self.course_elective}")
+            print(f"任課老師: {self.course_teacher}")
+            print(f"上課時間: {self.course_time}")
 
-    def get_course_time(self):
-        return self.course_time
-    
-    def get_teacher(self):
-        return self.course_teacher
-c1 = Course(course_id="G0D17M01",course_name="套裝軟體應用",course_hours="3",course_elective="選修",course_teacher="李宗儒",course_time="8:00-11:00")
+            def get_course_time(self):
+                return self.course_time
+            
+            def get_teacher(self):
+                return self.course_teacher
+    c1 = Course(course_id="G0D17M01",course_name="套裝軟體應用",course_hours="3",course_elective="選修",course_teacher="李宗儒",course_time="8:00-11:00")
 
-#列印課程資料
-c1.print_course_info()
+    #列印課程資料
+    c1.print_course_info()
 
-#查詢上課時間、查詢任課老師
+        #查詢上課時間、查詢任課老師
 print("上課時間:", c1.get_course_time())
 print("任課老師:", c1.get_teacher())
--- 1229/5.py
+++ 1229/5.py
@@ -4,6 +4,7 @@
 3.屬性(書名 書ID 作者 出版社 出版日期 是否借出 借出日期 還書期限 館藏位置 借書人姓名 學號 email)
 4.副函式
 """
+
 class library:
     def __init__(self,library_book,library_id,book_author,book_company,library_date,borrow,borrow_date,due_date,book_location,borrow_name,borrow_id,borrow_email):
         self.library_book = library_book
@@ -19,8 +20,51 @@ class library:
         self.borrow_id = borrow_id
         self.borrow_email = borrow_email
 
-    #l1=library('bookname','12345','作者')
-    #print("書名=",l1_library_book)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+def print_course_info(self):
+    print(f"書名: {self.library_book}")
+    print(f"書ID: {self.library_id}")
+    print(f"作者: {self.book_author}")
+    print(f"出版社: {self.book_company}")
+    print(f"出版日期: {self.library_date}")
+    print(f"是否借出: {self.borrow}")
+    print(f"借出日期: {self.borrow_date}")
+    print(f"還書期限: {self.due_date}")
+    print(f"館藏位置: {self.book_location}")
+    print(f"借書人姓名: {self.borrow_name}")
+    print(f"學號: {self.borrow_id}")
+    print(f"email: {self.borrow_email}")
+
+    l1=library('bookname','12345','作者')
+    print("書名=",l1_library_book)
+
+"""
+    
+
+
+
+
+
+
+    
 
 
    
